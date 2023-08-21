1692d6ca3580724f31a155b3406ab7ae8fb85acd
Dmitriy Bachromov <dvbak@yandex.ru>
2023-08-21 15:48:33 +0300

    fixed test_endpoints_1

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
 const urlRegAvatar = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
-const regPassword = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,20}$/;
+const regPassword = /^[a-zA-Z0-9]{8,}$/;
 
 module.exports = {
   urlRegAvatar,
